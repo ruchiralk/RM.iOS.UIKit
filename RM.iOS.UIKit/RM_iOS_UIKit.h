e497d0d2547e513c06b4f453dcdee157803af0f3
Ruchira Munidasa <ruchira.lk@gmail.com>
2019-08-15 20:46:54 +0530

    Initial Commit

---
+++ RM.iOS.UIKit/RM_iOS_UIKit.h
@@ -0,0 +1,19 @@
+//
+//  RM_iOS_UIKit.h
+//  RM.iOS.UIKit
+//
+//  Created by Ruchira Munidasa on 8/15/19.
+//  Copyright © 2019 Ruchira Munidasa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RM_iOS_UIKit.
+FOUNDATION_EXPORT double RM_iOS_UIKitVersionNumber;
+
+//! Project version string for RM_iOS_UIKit.
+FOUNDATION_EXPORT const unsigned char RM_iOS_UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RM_iOS_UIKit/PublicHeader.h>
+
+
